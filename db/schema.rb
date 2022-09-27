aea1c2a19c31ce7864cd8df420d39d2a8244a667
ismailarilik <arilik.ismail@gmail.com>
2022-09-27 10:29:12 +0300

    Create Devise model

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_26_082710) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_27_071822) do
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.integer "context"
@@ -30,6 +28,18 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_26_082710) do
     t.datetime "updated_at", null: false
     t.index ["category_id"], name: "index_transactions_on_category_id"
     t.index ["wallet_id"], name: "index_transactions_on_wallet_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   create_table "wallets", force: :cascade do |t|
