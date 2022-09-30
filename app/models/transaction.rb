10a111e6d267d3c7a0e7eed7177826678f4c48a6
ismailarilik <arilik.ismail@gmail.com>
2022-09-30 10:55:22 +0300

    Vectorize :price before_save for transactions
    Income prices should be positive, expense prices should be negative

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -4,4 +4,13 @@ class Transaction < ApplicationRecord
   belongs_to :category
   belongs_to :user
   belongs_to :wallet
+
+  before_save :vectorize_price
+
+  private
+
+  # Income prices should be positive, expense prices should be negative
+  def vectorize_price
+    self.price = self.category.income? ? self.price.abs : -(self.price.abs)
+  end
 end
